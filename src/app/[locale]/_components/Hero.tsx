d87a8851b612257611f03f9706fc46ae3ac0d7d8
Alejandro Mtz <alejandro.martinez.0598@gmail.com>
2025-09-23 13:17:24 -0600

    Animated header

--- src/app/[locale]/_components/Hero.tsx
+++ src/app/[locale]/_components/Hero.tsx
@@ -3,6 +3,7 @@
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 import { useCurrentLanguage } from './useCurrentLanguage';
+import AnimatedTittle from '~/app/_components/AnimatedTittle';
 
 export function Hero() {
   const t = useTranslations('hero');
@@ -11,14 +12,36 @@ export function Hero() {
   return (
     <section className="pt-24 pb-8 px-6 p">
       <div className="max-w-6xl mx-auto">
-        <div className="text-center mb-16">
-          {/* ANIMATION: Fade in from top with stagger effect */}
-          <h1 className="text-5xl md:text-7xl font-bold text-secondary-900 mb-6 font-canela">
-            {t('name')}
-          </h1>
-          <div className="text-2xl md:text-3xl text-primary-600 mb-8 font-medium font-canela">
-            {t('title')}
-          </div>
+        <div className="text-center mb-16 flex flex-col items-center">
+          <AnimatedTittle
+            text={t('name')}
+            className="text-5xl md:text-7xl font-bold text-secondary-900 mb-6 font-canela"
+            delay={100}
+            duration={0.5}
+            ease="elastic.out(1, 0.5)"
+            splitType="chars"
+            from={{ opacity: 1, y: 150 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.05}
+            rootMargin="-100px"
+            textAlign="center"
+            tag="h1"
+          />
+         <AnimatedTittle
+            text={t('title')}
+            className="text-2xl md:text-3xl text-primary-600 mb-8 font-medium font-canela"
+            delay={100}
+            duration={0.5}
+            ease="elastic.out(1, 0.5)"
+            splitType="chars"
+            from={{ opacity: 1, y: -150 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.05}
+            rootMargin="-100px"
+            textAlign="center"
+            tag="h2"
+          />
+
           <p className="text-xl text-secondary-600 max-w-3xl mx-auto leading-relaxed">
             {t('description')}
           </p>
